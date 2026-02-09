5d138b58266ab6b25d1a944525e79d2fc876cd33
Noa López <noaloprg@gmail.com>
2026-02-09 13:33:04 +0100

    Insercion de modulos de configuracion y TypeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST_PC'),
+        port: +configService.get('POSTGRES_PORT_PC'),
+        username: configService.get('POSTGRES_DOCKER_USER'),
+        password: configService.get('POSTGRES_DOCKER_ROOT_PASSWORD'),
+        database: `db_${configService.get('APP_NAME')}`,
+        //loads entities
+        autoLoadEntities: true,
+        //when changes in Entities are made, all tables are created as new
+        synchronize: true
+      }),
+      inject: [ConfigService]
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
