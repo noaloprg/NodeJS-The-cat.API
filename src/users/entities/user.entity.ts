45664df81e3535a4df28d8fb6d6b2b7bcfba08f5
Noa López <noaloprg@gmail.com>
2026-02-09 16:57:16 +0100

    Entity User basica (sin relaciones) creada

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,3 +1,34 @@
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { TipeUser } from "../enums/User-role.enum";
+import { UserLanguage } from "../enums/User-language.enum";
+
+@Entity()
 export class User {
-    
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column({ length: 150 })
+    mail: string
+
+    @Column({ length: 50, nullable: true })
+    name: string
+
+    @Column({ nullable: true })
+    password: string
+
+    @Column({ type: 'enum' })
+    role: TipeUser
+
+    @Column({ type: 'enum' })
+    language: UserLanguage
+
+    @CreateDateColumn()
+    createdAt: Date
+
+    @UpdateDateColumn()
+    updatedAt: Date
+
+    //may not be deleted
+    @DeleteDateColumn({ nullable: true })
+    deletedAt: Date
 }
