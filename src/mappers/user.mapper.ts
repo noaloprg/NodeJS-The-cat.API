8aa86a86eb5056d57b0e82140693b751bbdcb811
Noa López <noaloprg@gmail.com>
2026-02-10 18:40:18 +0100

    Mapper User creado con 2 metodos

---
+++ src/mappers/user.mapper.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@nestjs/common";
+import { CreateUserDto } from "src/users/dto/create-user.dto";
+import { ResponseUserDTO } from "src/users/dto/response-user.dto";
+import { User } from "src/users/entities/user.entity";
+
+@Injectable()
+export class UserMapper {
+    static createUserFromDTO(createDTO: CreateUserDto) {
+        const user = new User()
+        return Object.assign(user, createDTO)
+    }
+
+    static toResponseDTO(user: User) {
+        const response = new ResponseUserDTO()
+        return Object.assign(response, user)
+    }
+}
