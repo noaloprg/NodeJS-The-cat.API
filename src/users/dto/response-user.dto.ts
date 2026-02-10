681c8fa71790d7875f181edf3bb3741de0b43de0
Noa López <noaloprg@gmail.com>
2026-02-10 15:42:39 +0100

    DTO response y delete creados y completados

---
+++ src/users/dto/response-user.dto.ts
@@ -0,0 +1,13 @@
+import { UserType } from "../enums/User-role.enum"
+import { UserLanguage } from "../enums/User-language.enum"
+
+export class ResponseUserDTO {
+    id: number
+    mail: string
+    name: string
+    role: UserType
+    language: UserLanguage
+    createdAt: Date
+    updatedAt: Date
+    deletedAt: Date
+}
