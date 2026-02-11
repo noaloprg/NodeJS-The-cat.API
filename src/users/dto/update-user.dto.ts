322becff1439f9ffe362258dc98b9444ef39d05c
Noa López <noaloprg@gmail.com>
2026-02-11 10:41:19 +0100

    update-user.dto actualiza tambien language

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,6 +1,13 @@
-import { IsEmail, IsOptional, IsString, Max } from "class-validator";
+import { IsEmail, IsEnum, IsOptional, IsString, Max } from "class-validator";
 import { CreateUserDto } from "./create-user.dto";
+import { UserLanguage } from "../enums/User-language.enum";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class UpdateUserDto extends CreateUserDto {
-  
+    @ApiProperty({
+        enum : UserLanguage,
+    })
+    @IsEnum(UserLanguage)
+    @IsOptional()
+    language?: UserLanguage
 }
