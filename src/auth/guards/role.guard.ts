e03bdd8d7032263fbe51c8da110f4ab577b099f7
Noa López <noaloprg@gmail.com>
2026-02-11 18:35:09 +0100

    role.guard terminado sin probar

---
+++ src/auth/guards/role.guard.ts
@@ -0,0 +1,27 @@
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { UserType } from "src/users/enums/User-role.enum";
+import { ROLES_KEY } from "../decorators/role.decorator";
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+    constructor(
+        private reflector: Reflector
+    ) { }
+
+    canActivate(context: ExecutionContext): boolean {
+        //obtains all decorators from the class and methods
+        const requiredRoles =
+            this.reflector.getAllAndOverride<UserType[]>(ROLES_KEY, [context.getHandler(), context.getClass()])
+
+        if (!requiredRoles) return true;
+
+        //all info Httprequest 
+        const request = context.switchToHttp().getRequest()
+        //AuthGuard of user
+        const userRequest = request.user;
+
+        //role value verification
+        return requiredRoles.some((role) => userRequest.role === role)
+    }
+}
