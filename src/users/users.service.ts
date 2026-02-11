248478e3870ebc73818d0ce41c4a99f73c951e8d
Noa López <noaloprg@gmail.com>
2026-02-11 09:38:19 +0100

    User Repository insertado en el modulo y el servicio

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { UserMapper } from 'src/mappers/user.mapper';
 
 @Injectable()
 export class UsersService {
+  @InjectRepository(User)
+  private readonly repository: Repository<User>
+
   create(createUserDto: CreateUserDto) {
-    
+    const user = UserMapper.createUserFromDTO(createUserDto)
+    this.repository.save(user)
+    return UserMapper.toResponseDTO(user)
   }
 
   findAll() {
