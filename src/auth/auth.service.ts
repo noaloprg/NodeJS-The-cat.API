7dddd90d61cc01958464f571875b16fdc3f61d96
Noa López <noaloprg@gmail.com>
2026-02-12 12:21:56 +0100

    auth.service definido sin probar

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { ErrorMessages } from 'src/common/constants/error-messages';
+import { User } from 'src/users/entities/user.entity';
+import { UserSeeder } from 'src/users/entities/user.seeder';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
+    constructor(private jwtService: JwtService, private userService: UsersService) { }
 
+    // JWTModule uses .registerAsync()
+    async login(mail: string, pass: string) {
+        const userRequested = await this.userService.getUserByMail(mail)
+
+        if (!userRequested || userRequested.password !== pass) throw new UnauthorizedException('Access denied');
+
+        //contains user info
+        const payload = {
+            sub: userRequested.id,
+            mail: userRequested.mail,
+            role: userRequested.role
+        };
+        return {
+            // header (algorithm in JwtService) + payload + signature (secret JwtService)
+            access_token: await this.jwtService.signAsync(payload)
+        }
+    }
 }
