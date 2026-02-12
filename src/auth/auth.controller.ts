34f4fdb8e513ca8f8135cee21ad2ab3498c11087
Noa López <noaloprg@gmail.com>
2026-02-12 16:25:09 +0100

    login.dto aplica ApiProperty
    auth.controller implementa endpoint de login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,13 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LoginDTO } from './dto/login.dto';
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
 
+  @Post('login')
+  async login(@Body() loginDTO: LoginDTO) {
+    //returns access token 
+    return this.authService.login(loginDTO)
+  }
 }
--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,10 +1,17 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsString, MaxLength, MinLength } from "class-validator";
 
 export class LoginDTO {
+    @ApiProperty({
+        example: 'user@gmail.com'
+    })
     @IsEmail()
     @MaxLength(150)
     mail: string
 
+    @ApiProperty({
+        example: '12345678'
+    })
     @IsString()
     @MinLength(8)
     password: string
