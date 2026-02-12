f066c12c9c0edc1dc3c9ea09f6def5d115472b1d
Noa López <noaloprg@gmail.com>
2026-02-12 18:10:14 +0100

    Createverifitacion.dto creado

--- src/verification/dto/create-verification.dto.ts
+++ src/verification/dto/create-verification.dto.ts
@@ -1,1 +1,21 @@
-export class CreateVerificationDto {}
+import { IsEmail, IsString, MaxLength, MinLength } from "class-validator"
+import { UserLanguage } from "src/users/enums/User-language.enum"
+import { Column, CreateDateColumn, UpdateDateColumn, DeleteDateColumn } from "typeorm"
+
+export class CreateVerificationDto {
+    //no APIPorperty / no endpoints
+    @IsEmail()
+    @MaxLength(150)
+    targetEmail: string
+
+    @IsString()
+    verificationToken: string
+
+    @IsString()
+    @MaxLength(50)
+    name?: string | null
+
+    @IsString()
+    @MinLength(8)
+    password?: string | null
+}
