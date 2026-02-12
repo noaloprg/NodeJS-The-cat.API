3b6f048be7ce19c3e4c6be73fac7ff1f94e6d50b
Noa López <noaloprg@gmail.com>
2026-02-12 11:08:50 +0100

    auth.module implementa el modulo JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [UsersModule,
+    JwtModule.registerAsync({
+      //JWT config
+      inject: [ConfigService],
+      global: true,
+
+      //token config
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: '120s'
+        }
+      })
+    })
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
-export class AuthModule {}
+export class AuthModule { }
