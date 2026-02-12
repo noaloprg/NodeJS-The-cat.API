316210f73384f389f1953a2382ba4fb19861041c
Noa López <noaloprg@gmail.com>
2026-02-12 13:47:57 +0100

    jwt.strategy terminado sin probar

---
+++ src/auth/verification/jwt.strategy.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(configService: ConfigService) {
+        super({
+            //info token
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: configService.get<string>('JWT_SECRET') || '',
+        });
+    }
+    async validate(payload: any) {
+        return {
+            id: payload.sub,
+            mail: payload.mail,
+            role: payload.role
+        }
+    }
+}
