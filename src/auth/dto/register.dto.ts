ba28638971928459c5484962d73549fc090a893d
Noa López <noaloprg@gmail.com>
2026-02-13 10:48:49 +0100

    register.dto implementa ApiProperty

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,14 +1,24 @@
+import { ApiProperty } from "@nestjs/swagger"
 import { IsEmail, IsString, MaxLength, MinLength } from "class-validator"
 
 export class RegisterDTO {
+    @ApiProperty({
+        example: 'user@gmail.com'
+    })
     @IsEmail()
     @MaxLength(150)
     targetEmail: string
 
+    @ApiProperty({
+        example: 'person'
+    })
     @IsString()
     @MaxLength(50)
     name?: string | null
 
+    @ApiProperty({
+        example: '12345678'
+    })
     @IsString()
     @MinLength(8)
     password?: string | null
