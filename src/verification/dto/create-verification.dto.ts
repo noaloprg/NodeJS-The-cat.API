5d3f25c926bd2bf1c92306c11c28f6c904067583
Noa López <noaloprg@gmail.com>
2026-02-13 10:26:00 +0100

    create-verif hereda de Register.dto

--- src/verification/dto/create-verification.dto.ts
+++ src/verification/dto/create-verification.dto.ts
@@ -1,21 +1,4 @@
-import { IsEmail, IsString, MaxLength, MinLength } from "class-validator"
-import { UserLanguage } from "src/users/enums/User-language.enum"
-import { Column, CreateDateColumn, UpdateDateColumn, DeleteDateColumn } from "typeorm"
+import { RegisterDTO } from "src/auth/dto/register.dto"
 
-export class CreateVerificationDto {
-    //no APIPorperty / no endpoints
-    @IsEmail()
-    @MaxLength(150)
-    targetEmail: string
-
-    @IsString()
-    verificationToken: string
-
-    @IsString()
-    @MaxLength(50)
-    name?: string | null
-
-    @IsString()
-    @MinLength(8)
-    password?: string | null
+export class CreateVerificationDto extends RegisterDTO {
 }
