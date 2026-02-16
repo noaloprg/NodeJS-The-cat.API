7872b1f29120d2b25be3f6f0fb3a4b86ba39a6f4
Noa López <noaloprg@gmail.com>
2026-02-16 13:39:01 +0100

    feat (modulos): importan las entidades en modulo de TypeOrm

--- src/breed/breed.module.ts
+++ src/breed/breed.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { BreedService } from './breed.service';
 import { BreedController } from './breed.controller';
+import { Breed } from './entities/breed.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Breed]),
+  ],
   controllers: [BreedController],
   providers: [BreedService],
 })
-export class BreedModule {}
+export class BreedModule { }
--- src/pet/pet.module.ts
+++ src/pet/pet.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PetService } from './pet.service';
 import { PetController } from './pet.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Pet } from './entities/pet.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Pet]),
+  ],
   controllers: [PetController],
   providers: [PetService],
 })
-export class PetModule {}
+export class PetModule { }
