c610c79b71de147d4170e8ec9772b8c48a784b7f
Noa López <noaloprg@gmail.com>
2026-02-16 12:48:51 +0100

    feat(controllers): elimina enpoints innecesarios

--- src/pet/pet.controller.ts
+++ src/pet/pet.controller.ts
@@ -7,28 +7,5 @@ import { UpdatePetDto } from './dto/update-pet.dto';
 export class PetController {
   constructor(private readonly petService: PetService) {}
 
-  @Post()
-  create(@Body() createPetDto: CreatePetDto) {
-    return this.petService.create(createPetDto);
-  }
 
-  @Get()
-  findAll() {
-    return this.petService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.petService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePetDto: UpdatePetDto) {
-    return this.petService.update(+id, updatePetDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.petService.remove(+id);
-  }
 }
