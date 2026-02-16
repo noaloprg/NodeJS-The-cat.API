907498d4942f8b1e38bce4ec46d479a433917c69
Noa López <noaloprg@gmail.com>
2026-02-16 12:04:22 +0100

    feat(pet.entity, user.entity): entidad pet creada y relacionada con user

--- src/pet/entities/pet.entity.ts
+++ src/pet/entities/pet.entity.ts
@@ -1,1 +1,29 @@
-export class Pet {}
+import { MaxLength } from "class-validator";
+import { User } from "src/users/entities/user.entity";
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class Pet {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column({ length: 50 })
+    petName: string
+
+    @CreateDateColumn({ type: 'timestamptz' })
+    createdAt: Date
+
+    @UpdateDateColumn({ type: 'timestamptz' })
+    updatedAt: Date
+
+    @DeleteDateColumn({ type: 'timestamptz', nullable: true })
+    deletedAt?: Date | null
+
+    //if user is deleted, owner is Null
+    @ManyToOne(() => User, (u) => u.pets, { onDelete: "SET NULL" })
+    @JoinColumn({ name: 'ownerId' })
+    owner: User
+
+    @Column()
+    animalId: number
+}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,6 +1,7 @@
-import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { UserType } from "../enums/User-role.enum";
 import { UserLanguage } from "../enums/User-language.enum";
+import { Pet } from "src/pet/entities/pet.entity";
 
 @Entity()
 export class User {
@@ -11,10 +12,10 @@ export class User {
     mail: string
 
     @Column({ type: 'varchar', length: 50 })
-    name: string 
+    name: string
 
-    @Column({ type: 'varchar'})
-    password: string 
+    @Column({ type: 'varchar' })
+    password: string
 
     @Column({ type: 'enum', enum: UserType, default: UserType.USER })
     role: UserType
@@ -30,4 +31,8 @@ export class User {
 
     @DeleteDateColumn({ type: 'timestamptz', nullable: true })
     deletedAt?: Date | null
+
+    //can not have pets
+    @OneToMany(() => Pet, (p) => p.owner)
+    pets: Pet[]
 }
