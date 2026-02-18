08746ef2d87370d85ef284d57b8f76d215e4d64e
Noa López <noaloprg@gmail.com>
2026-02-18 12:02:49 +0100

    feat(breed.service): inyecta repositorio y comprueba existencia a traves de id externo

--- src/breed/breed.service.ts
+++ src/breed/breed.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBreedDto } from './dto/create-breed.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Breed } from './entities/breed.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class BreedService {
+  @InjectRepository(Breed)
+  private readonly repository: Repository<Breed>
+
   create(createBreedDto: CreateBreedDto) {
     return 'This action adds a new breed';
   }
@@ -18,5 +24,13 @@ export class BreedService {
 
   remove(id: number) {
     return `This action removes a #${id} breed`;
+  }
+
+  existsByExternalId(exId: string) {
+    const breed = this.repository.findOneBy({ externalId: exId })
+
+    let exists = true
+    if (!breed) exists = false
+    return exists
   }
 }
