2ecc00ac726574c447d3ecd9881c8f2b3cfc0b61
Noa López <noaloprg@gmail.com>
2026-02-18 11:54:10 +0100

    feat(cat-breed.dto): crea dto para crear cat y breed junto de lo que reciba de la peticion de la API

---
+++ src/cat/dto/create-cat-breed.dto.ts
@@ -0,0 +1,10 @@
+import { CreateCatDto } from "./create-cat.dto";
+import { CreateBreedDto } from "src/breed/dto/create-breed.dto";
+
+/* 
+CreateCatDTO (1 cat) with List of CreateBreedDTO (many breeds)
+Creates 1 cat and many breeds at the same time
+*/
+export class CreateCatWithBreedDTO extends CreateCatDto {
+    listBreeds: CreateBreedDto[]
+}
