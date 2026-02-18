977ba24e487e1ee59933b5c50d59ae9924c62f73
Noa López <noaloprg@gmail.com>
2026-02-18 15:57:33 +0100

    feat(cat.controller): obtiene gatos de la api externa  y obtiene los gatos de la propia

--- src/cat/cat.controller.ts
+++ src/cat/cat.controller.ts
@@ -1,7 +1,28 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { CatService } from './cat.service';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import { Roles } from 'src/common/decorators/role.decorator';
+import { UserType } from 'src/users/enums/User-role.enum';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/role.guard';
+import { BEARER_KEY } from 'src/common/constants/keys';
 
 @Controller('cat')
 export class CatController {
-  constructor(private readonly catService: CatService) {}
+  constructor(private readonly catService: CatService) { }
+
+  @ApiBearerAuth(BEARER_KEY)
+  @Get('api:amount')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserType.ADMIN)
+  @ApiOperation({ summary: 'Obtiene gatos de The Cat API' })
+  async getCats(@Param('amount') amount: number = 1) {
+    return await this.catService.createCatBreedFromApi(amount)
+  }
+
+  @Get()
+  @ApiOperation({ summary: 'Obtiene todos los gatos de la BBDD' })
+  async getAll() {
+    return await this.catService.getAllCats()
+  }
 }
