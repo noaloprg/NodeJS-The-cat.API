16b11d8545c168bd9070973f49af8786a7826cdc
Noa López <noaloprg@gmail.com>
2026-02-18 13:58:37 +0100

    feat(breed.service): metodo que actualiza relaciones con cats

--- src/breed/breed.service.ts
+++ src/breed/breed.service.ts
@@ -4,6 +4,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Breed } from './entities/breed.entity';
 import { Repository } from 'typeorm';
 import { BreedMapper } from 'src/common/mappers/breed.mapper';
+import { Cat } from 'src/cat/entities/cat.entity';
 
 @Injectable()
 export class BreedService {
@@ -24,6 +25,22 @@ export class BreedService {
     //doesnt throw exception if exists
   }
 
+  async updateRelations(id: number, cat: Cat) {
+    //loads breed with the cats related
+    const breed = await this.repository.findOne({
+      where: {id},
+      relations: ['cats']
+  })
+
+    if (breed) {
+      //if its empty creates array 
+      if (!breed.cats) breed.cats = []
+
+      breed.cats.push(cat)
+      this.repository.save(breed)
+    }
+  }
+  
   async findAll() {
     const allBreeds = await this.repository.find()
     return allBreeds.map(b => this.mapper.toResponseDTO(b))
