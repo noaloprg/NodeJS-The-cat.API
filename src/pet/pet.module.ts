325db4790d275421e5f5c610fb4beab3a01b14e1
Noa López <noaloprg@gmail.com>
2026-02-18 17:34:16 +0100

    feat(cat): module importa modulo de user y cat, service inyecta seriicos, mapper y repositorio

--- src/pet/pet.module.ts
+++ src/pet/pet.module.ts
@@ -5,11 +5,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Pet } from './entities/pet.entity';
 import { CatModule } from 'src/cat/cat.module';
 import { PetMapper } from 'src/common/mappers/pet.mapper';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Pet]),
     CatModule,
+    UsersModule,
     PetMapper
   ],
   controllers: [PetController],
--- src/pet/pet.service.ts
+++ src/pet/pet.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePetDto } from './dto/create-pet.dto';
 import { UpdatePetDto } from './dto/update-pet.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Pet } from './entities/pet.entity';
+import { Repository } from 'typeorm';
+import { PetMapper } from 'src/common/mappers/pet.mapper';
+import { UsersService } from 'src/users/users.service';
+import { CatService } from 'src/cat/cat.service';
 
 @Injectable()
 export class PetService {
+  @InjectRepository(Pet)
+  private readonly repository: Repository<Pet>
+
+  constructor(
+    private readonly mapper: PetMapper,
+    private readonly userService: UsersService,
+    private readonly catService: CatService,
+  ) { }
+
   create(createPetDto: CreatePetDto) {
     return 'This action adds a new pet';
   }
