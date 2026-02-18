f1e62884466c3f128c828183b91eaa62dc657db4
Noa López <noaloprg@gmail.com>
2026-02-18 12:34:36 +0100

    feat(cat.service): crea metodo que obtiene cats y breeds de la Api cat

--- src/cat/cat.service.ts
+++ src/cat/cat.service.ts
@@ -1,12 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
+
 export class CatService {
   constructor(
     private readonly httpService: HttpService
   ) { }
+
   create(createCatDto: CreateCatDto) {
     return 'This action adds a new cat';
   }
@@ -22,5 +25,24 @@ export class CatService {
 
   remove(id: number) {
     return `This action removes a #${id} cat`;
+  }
+
+  // * CATS + BREEDS RANDOM
+  // amount -> num of cats admin requests
+  private async getCatsFromApi(amount: number) {
+    try {
+      /*
+      tranforms Observable into Promise<AxiosResponse> to access AxiosResponse methods
+      */
+      const apiReponse = await firstValueFrom(this.httpService.get('/images/search', {
+        params: { limit: amount }
+      }))
+      return apiReponse.data;
+    }
+    catch (error) {
+      const status = error?.status || HttpStatus.INTERNAL_SERVER_ERROR;
+      const message = error?.message || 'Error al conectar con Cat API';
+      throw new HttpException(message, status);
+    }
   }
 }
