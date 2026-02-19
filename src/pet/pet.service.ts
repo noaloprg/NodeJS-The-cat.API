df17f3692dcb1ace67d6af814ed35d975c2ba80a
Noa López <noaloprg@gmail.com>
2026-02-19 13:09:19 +0100

    refactor(pet.service): relacion automatica entre entidades a traves de create()
    fix(pet.service): verifica existencia de cat correctamente

--- src/pet/pet.service.ts
+++ src/pet/pet.service.ts
@@ -7,11 +7,7 @@ import { Repository } from 'typeorm';
 import { PetMapper } from 'src/common/mappers/pet.mapper';
 import { UsersService } from 'src/users/users.service';
 import { CatService } from 'src/cat/cat.service';
-import { Cat } from 'src/cat/entities/cat.entity';
-import { User } from 'src/users/entities/user.entity';
 import { ErrorMessages } from 'src/common/constants/error-messages';
-import { ExceptionsHandler } from '@nestjs/core/exceptions/exceptions-handler';
-import { NotFoundError } from 'rxjs';
 
 @Injectable()
 export class PetService {
@@ -28,62 +24,26 @@ export class PetService {
 
   //userId comes from @Req in controller
   async createPetRelated(createPetDto: CreatePetDto, userId: number) {
+    //validates user exists
     const user = await this.userService.checkExistanceById(userId)
+    //validates cat  exists
+    const cat = await this.catService.findEntityById(createPetDto.animalId)
+    //verifies that the cat is not a pet
+    this.checkExistanceCat(cat.id)
 
-    if (user) {
-      const cat = await this.catService.findEntityById(createPetDto.animalId)
-      const pet = await this.createSimplePet(user, cat, createPetDto.petName)
-
-      //relations WITH pet 
-      await this.userService.updateRelationPet(user.id, pet)
-      await this.catService.updateRelationPet(cat.id, pet)
-
-      //relactions OF pet
-      await this.updateRelationCat(pet.id, cat)
-      await this.updateRelationUser(pet.id, user)
-      return await this.mapper.toResponseDTO(pet)
-    }
+    const pet = await this.repository.create({
+      petName: createPetDto.petName,
+      //relates to cat and user
+      cat: cat,
+      owner: user,
+    })
+    await this.repository.save(pet)
+    return await this.mapper.toResponseDTO(pet)
   }
-
-
-  private async updateRelationCat(id: number, cat: Cat) {
-    const pet = await this.repository.findOneBy({ id: id })
-
-    if (pet) {
-      if (!pet.cat) {
-        pet.cat = cat
-        await this.repository.save(pet)
-      }
-      else throw new BadRequestException('This cat is already a pet')
-    }
-    else throw new NotFoundException(ErrorMessages.notFoundByIdMessage(this.ENTITY_NAME, id))
-  }
-
-
-  private async updateRelationUser(id: number, user: User) {
-    const pet = await this.repository.findOneBy({ id: id })
-
-    if (pet) {
-      if (!pet.owner) {
-        pet.owner = user
-        await this.repository.save(pet)
-      }
-      else throw new BadRequestException('This pet has already a owner')
-    }
-    else throw new NotFoundException(ErrorMessages.notFoundByIdMessage(this.ENTITY_NAME, id))
-  }
-
-  private async checkExistance(animalId: number) {
-    const petRequested = await this.repository.findOneBy({ id: animalId })
+  private async checkExistanceCat(animalId: number) {
+    //list insisde a list
+    const petRequested = await this.repository.findOneBy({ cat: { id: animalId } })
     if (petRequested) throw new ConflictException(ErrorMessages.alreadyRegistered('pet'))
   }
 
-  private async createSimplePet(user: User, cat: Cat, name: string) {
-    //creates pet from mapper
-    const pet = this.mapper.createPet(user, cat, name)
-    //checks if that cat is already a Pet, if not exception
-    await this.checkExistance(pet.cat.id)
-    //creates Pet 
-    return await this.repository.save(pet)
-  }
 }
